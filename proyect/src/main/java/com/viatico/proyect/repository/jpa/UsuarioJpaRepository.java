b620f0e6f9b6590a335dc47d4bf190968d1c0d3c
Russel-Fs <russelfloressolano900@gmail.com>
2025-12-29 16:25:00 -0500

    feat: Introduce user and employee management, security, data initialization, and a request API endpoint.

--- proyect/src/main/java/com/viatico/proyect/repository/jpa/UsuarioJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.viatico.proyect.repository.jpa;
-
-import com.viatico.proyect.entity.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UsuarioJpaRepository extends JpaRepository<Usuario, Long> {
-    Optional<Usuario> findByUsername(String username);
-}
